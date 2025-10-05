931b6434b0b3ff717f3d916cb459bf1d17368923
RenzoFro34 <renzof@usf.edu>
2025-10-05 03:08:33 -0400

    Add files via upload
    From about halfway through step 3 thru  step 6

--- cc6.js
+++ cc6.js
@@ -5,26 +5,47 @@ class Employee {
     }
    
     describe() {
-        return "${this.name} works in the ${this.department} department";
+        return `${this.name} works in the ${this.department} department`;
     }
     }
 
-class Managers extends Employee {
+class Manager extends Employee {
  constructor(name, department, teamSize) {
 super(name, department);
 this.teamSize = teamSize;}
 
 describe() {
-    return "${this.name} is the manager of ${this.teamSize} employees in the ${this.department} department";
+    return `${this.name} is the manager of ${this.teamSize} employees in the ${this.department} department`;
 }
  }
 
  const employees = [
-    new employee("Genevieve", "Bakery"),
-    new employee("Kevin", "Bakery"),
-    new employee("Patricia", "Apparel"),
-    new employee("Sam", "Apparel"),
-    new employee("Penelope", "Produce")
-    new employee("Tony", "Produce")
+    new Employee("Genevieve", "Bakery"),
+    new Employee("Chris", "Bakery"),
+    new Employee("Patricia", "Apparel"),
+    new Employee("Sam", "Apparel"),
+    new Employee("Penelope", "Produce"),
+    new Employee("Alex", "Produce")
  ];
 
+const managers = [
+    new Manager("Karla", "Bakery", 12),
+    new Manager("Jerry", "Apparel",11),
+    new Manager("Harrison", "Produce", 34)
+];
+
+class Company {
+    constructor() {
+        this.employees = []};
+       addEmployee(employee) {
+        this.employees.push(employee);
+       };
+    listEmployees() {
+    this.employees.forEach(Employee => {console.log(Employee.describe())});}
+};
+
+const company = new Company()
+employees.forEach(Employee => company.addEmployee(Employee));
+managers.forEach(Manager => company.addEmployee(Manager));
+
+company.listEmployees();
